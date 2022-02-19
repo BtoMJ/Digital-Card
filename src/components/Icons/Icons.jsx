8d13cf8b46fabf9f27c02c26cee3397c41b90a27
BtoMJ <danny_mj17@hotmail.com>
2022-02-18 22:27:09 -0600

    Actualizando

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -11,7 +11,7 @@ import logoUnidos from '../../assets/ICN-UNIDOS.png';
 import logoGobierna from '../../assets/ICN_GOBIERNA.png';
 
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
